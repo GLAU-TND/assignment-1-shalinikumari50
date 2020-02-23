6f3bb38d5e99887e32f7874057eafa23b0c11a03
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-23 21:37:10 +0530

    add getter and setter methods

--- src/person/Person.java
+++ src/person/Person.java
@@ -14,4 +14,36 @@ public class Person {
         this.myContactList = myContactList;
         this.emailAddress = emailAddress;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public MyContactList getMyContactList() {
+        return myContactList;
+    }
+
+    public void setMyContactList(MyContactList myContactList) {
+        this.myContactList = myContactList;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
