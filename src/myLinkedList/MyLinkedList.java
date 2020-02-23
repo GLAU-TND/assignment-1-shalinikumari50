5f918a0d82aae39f0e4e8c3106cc7d172f75c51e
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-23 18:23:25 +0530

    add method : getNode(position : int) (Node<T>) to return the node present at the specified position

--- src/myLinkedList/MyLinkedList.java
+++ src/myLinkedList/MyLinkedList.java
@@ -57,6 +57,22 @@ public class MyLinkedList<T> {
         }
     }
 
+    public Node<T> getNode(int position) {
+        int count = 0;
+        if (head == null) {
+            return null;
+        }
+        Node<T> temp = head;
+        while (temp != null) {
+            count++;
+            if (count == position) {
+                return temp;
+            }
+            temp = temp.getNext();
+        }
+        return null;
+    }
+
 
 
 }
